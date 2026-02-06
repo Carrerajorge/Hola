64cf1b7d4acdf1802d3ef9c25014afadb08ad70a
Jorge Carrera <carrerajorge874@gmail.com>
2026-02-06 23:23:40 +0400

    chore(mobile): scaffold Expo app (EAS-ready) in apps/mobile

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,35 @@
+import type { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'Iliagpt',
+  slug: 'iliagpt',
+  scheme: 'iliagpt',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: './assets/splash-icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#0f172a',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    // TODO: set to your Apple bundle identifier
+    bundleIdentifier: 'com.iliagpt.app',
+  },
+  android: {
+    // TODO: set to your Android applicationId
+    package: 'com.iliagpt.app',
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#0f172a',
+    },
+  },
+  extra: {
+    apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL ?? 'https://iliagpt.com',
+  },
+};
+
+export default config;
